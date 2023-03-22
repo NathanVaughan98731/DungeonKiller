a3368d8fdfbae13c109df72b66a5f3581c9a675e
NathanVaughan123 <nathanjvaughan13@gmail.com>
2023-03-22 23:27:58 +1100

    Added more helper functionality for other files.

--- Assets/Scripts/Utilities/HelperUtilities.cs
+++ Assets/Scripts/Utilities/HelperUtilities.cs
@@ -14,6 +14,17 @@ public static class HelperUtilities
         return false;
     }
 
+    // Null value debug check
+    public static bool ValidateCheckNullValue(Object thisObject, string fieldName, Object objectToCheck)
+    {
+        if (objectToCheck == null)
+        {
+            Debug.Log(fieldName + " is null and must contain a value in object " + thisObject.name.ToString());
+            return true;
+        }
+        return false;
+    }
+
     // List empty or contains null value check - returns true if there is an error
     public static bool ValidateCheckEnumerableValues(Object thisObject, string fieldName, IEnumerable enumerableObjectToCheck)
     {
@@ -47,4 +58,29 @@ public static class HelperUtilities
 
         return error;
     }
+
+    // Positive value debug check - if zero is allowed set isZeroAllowed to true. Returns true if there is an error
+    public static bool ValidateCheckPositiveValue(Object thisObject, string fieldName, int valueToCheck, bool isZeroAllowed)
+    {
+        bool error = false;
+
+        if (isZeroAllowed)
+        {
+            if (valueToCheck < 0)
+            {
+                Debug.Log(fieldName + " must contain a positive value or zero in object " + thisObject.name.ToString());
+                error = true;
+            }
+        }
+        else
+        {
+            if (valueToCheck <= 0)
+            {
+                Debug.Log(fieldName + " must contain a positive value in object " + thisObject.name.ToString());
+                error = true;
+            }
+        }
+        return error;
+    }
+
 }
