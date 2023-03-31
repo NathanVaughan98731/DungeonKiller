543f7022a64db5beb81df0acbcf22f156e4a0252
NathanVaughan123 <nathanjvaughan13@gmail.com>
2023-03-31 13:13:02 +1100

    Doors now open and close in game when the player goes near it.

--- Assets/Scripts/Misc/Settings.cs
+++ Assets/Scripts/Misc/Settings.cs
@@ -34,5 +34,16 @@ public static class Settings
     public static int rollDown = Animator.StringToHash("rollDown");
     public static int rollLeft = Animator.StringToHash("rollLeft");
     public static int rollRight = Animator.StringToHash("rollRight");
+
+    // Animator parameters - Door
+    public static int open = Animator.StringToHash("open");
+
     #endregion
+
+    #region GAMEOBJECT TAGS
+    public const string playerTag = "Player";
+    public const string playerWeapon = "playerWeapon";
+    #endregion
+
+
 }
