c67581a76b4562381873e0e00abd58b7967517f3
NathanVaughan123 <nathanjvaughan13@gmail.com>
2023-07-06 17:34:03 +1000

    Modified the Health class to consider damage.

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -2,11 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(HealthEvent))]
 [DisallowMultipleComponent]
 public class Health : MonoBehaviour
 {
     private int startingHealth;
     private int currentHealth;
+    private HealthEvent healthEvent;
+
+    [HideInInspector] public bool isDamageable = true;
+
+    private void Awake()
+    {
+        // Load components
+        healthEvent = GetComponent<HealthEvent>();
+    }
+
+    private void Start()
+    {
+        // Trigger the health event to update UI
+        CallHealthEvent(0);
+    }
+
+    // Use this method to apply damage
+    public void TakeDamage(int damageAmount)
+    {
+        if (isDamageable)
+        {
+            currentHealth -= damageAmount;
+            CallHealthEvent(damageAmount);
+        }
+    }
+
+    private void CallHealthEvent(int damageAmount)
+    {
+        // Trigger the health event
+        healthEvent.CallHealthChangedEvent(((float)currentHealth / (float)startingHealth), currentHealth, damageAmount);
+    }
 
     // Set starting health
     public void SetStartingHealth(int startingHealth)
